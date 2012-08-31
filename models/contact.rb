41af666e6339533f5c3ad51ab006af4c95383f0a
David Julia <dajulia3@gmail.com>
2012-08-31 14:16:00 -0600

    Added contact.rb to the repo

---
+++ models/contact.rb
@@ -0,0 +1,26 @@
+class Contact < ContactsRecordBase
+  self.table_name  = 'ABPerson'
+  self.primary_key = 'ROWID'
+  has_many :numbers, :foreign_key => :record_id
+
+  #Make the names prettier
+  alias_attribute :first_name, :First
+  alias_attribute :last_name, :Last
+
+  def self.alphabetical_contacts
+    #leave out contacts with no name -- I think those are reserved contacts for stuff apple does
+    self.order("Last ASC, First ASC").all.reject{|ctct| ctct.first_name.nil? && ctct.last_name.nil?}
+  end
+
+  def full_name
+    "#{self.first_name unless self.first_name.nil?}"+" #{self.Last unless self.last_name.nil?}"
+  end
+
+  def messages
+    msgs =[]
+    numbers.each{|num| msgs += num.messages}
+    msgs
+  end
+
+end
+
